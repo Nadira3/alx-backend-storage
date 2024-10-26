11a7dd9250987209410607fb48931b539a40fede
Nadira3 <paitanun35@gmail.com>
2024-10-26 10:26:51 +0100

    edited file that includes command to import file

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,9 @@
 -- Script that creates an index idx_name_first on the table names and the first letter of name
 
+-- import data rows
+SOURCE ./names.sql
 -- Drop index first if it exists
 DROP INDEX IF EXISTS idx_name_first ON names;
 
 -- Create index on the first letter of the name column
-CREATE INDEX idx_name_first ON names (name(1));
+CREATE INDEX idx_name_first ON names (name(1) ASC);

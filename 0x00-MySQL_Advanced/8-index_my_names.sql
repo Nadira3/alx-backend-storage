50300b2935d349a403e6b1dfb8b03c6a323435a0
Nadira3 <paitanun35@gmail.com>
2024-10-26 10:43:44 +0100

    correction: fixed syntax error on index statement

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Script that creates an index idx_name_first on the table names and the first letter of name
 
 -- Create index on the first letter of the name column
-CREATE INDEX idx_name_first ON names (name(1), ASC);
+CREATE INDEX idx_name_first ON names (name(1));

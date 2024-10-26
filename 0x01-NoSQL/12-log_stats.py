3b2bf961701938de964495635602afb1cf69b75b
Nadira3 <paitanun35@gmail.com>
2024-10-26 21:33:59 +0100

    added conditional statement for script to run

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """ module that contains database script """
 
-
-from pymongo import MongoClient
-
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-
-# Print the total count of logs
-print(f"{collection.count_documents({})} logs")
-
-# Count the number of each HTTP method
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    count = collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
-
-status_count = collection.count_documents({"path": "/status"})
-print(f"{status_count} status check")
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    
+    # Print the total count of logs
+    print(f"{collection.count_documents({})} logs")
+    
+    # Count the number of each HTTP method
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    
+    status_count = collection.count_documents({"path": "/status"})
+    print(f"{status_count} status check")

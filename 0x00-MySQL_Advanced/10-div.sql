b279af1a980b1d8d6af4d5710e750cd103009d1e
Nadira3 <paitanun35@gmail.com>
2024-10-26 11:38:17 +0100

    removed drop function statement to prevent conflict in test environment

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,9 +1,6 @@
 -- SQL script that creates a function SafeDiv that divides (and returns) the first
 -- by the second number or returns 0 if the second number is equal to 0
 
--- select database;
-DROP FUNCTION IF EXISTS SafeDiv;
-
 -- Set delimiter to avoid conflict in parsing statement
 DELIMITER //
 

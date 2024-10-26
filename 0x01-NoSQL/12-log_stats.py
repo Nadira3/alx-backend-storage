45b7880e7d635779a59c7d53c18132957a6d3035
Nadira3 <paitanun35@gmail.com>
2024-10-26 21:27:20 +0100

    task 12 first submission

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.logs
+collection = db.nginx
+
+# Print the total count of logs
+print(f"{collection.count_documents({})} logs")
+
+# Count the number of each HTTP method
+print("Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    count = collection.count_documents({"method": method})
+    print(f"\tmethod {method}: {count}")
+
+status_count = collection.count_documents({"path": "/status"})
+print(f"{status_count} status check")

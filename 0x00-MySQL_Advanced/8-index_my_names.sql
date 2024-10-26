30090435df5c98cef4896aeccf57a973ffa8818f
Nadira3 <paitanun35@gmail.com>
2024-10-26 10:29:50 +0100

    removed asc from key feature of column name

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,9 +1,7 @@
 -- Script that creates an index idx_name_first on the table names and the first letter of name
 
--- import data rows
-SOURCE ./names.sql
 -- Drop index first if it exists
 DROP INDEX IF EXISTS idx_name_first ON names;
 
 -- Create index on the first letter of the name column
-CREATE INDEX idx_name_first ON names (name(1) ASC);
+CREATE INDEX idx_name_first ON names (name(1));

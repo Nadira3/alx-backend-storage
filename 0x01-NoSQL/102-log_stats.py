8d98febb80927853866c8cddf238fb51493c32b2
Nadira3 <paitanun35@gmail.com>
2024-10-27 06:33:24 +0100

    task 15 first submission

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" module that contains database script """
+
+
+def main():
+    """ main function housing the script logic """
+    from pymongo import MongoClient
+
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+
+    # Print the total count of logs
+    print(f"{collection.count_documents({})} logs")
+
+    # Count the number of each HTTP method
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_count = collection.count_documents({"path": "/status"})
+    print(f"{status_count} status check")
+
+    # Aggregation pipeline to count occurrences of each IP
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}  # Sort by count in descending order
+    ]
+
+    results = collection.aggregate(pipeline)
+    print("IPs:")
+    for result in results:
+        print(f"{result['_id']}: {result['count']}")
+
+
+if __name__ == "__main__":
+    main()
